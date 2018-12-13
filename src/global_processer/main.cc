a6338e84ae1345770df227d7d4bec6b15ae178ad
WangQing <kingchin1218@126.com>
2018-12-14 01:00:34 +0800

    Add cmake

--- src/global_processer/main.cc
+++ src/global_processer/main.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 
+#include "handy/handy.h"
+
 int main(int argc, char *argv[]) {
+    handy::EventBase base;
     std::cout << "Hello this is worker." << std::endl;
     return 0;
 }
